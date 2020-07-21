288ea752b8c7fcc3efc4d0b47a8e317bf8c33a40
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2020-07-21 13:16:09 -0700

    Somthing
    IDK

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const DabiImages = require("dabi-images");
+const DabiClient = new DabiImages.Client();
+const prefix = 't/';
+var url 
+
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("yes master toast");
+  } 
+  if (message.content.startsWith(prefix + "toastpic")) {
+    message.channel.send("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Toast-2.jpg/1024px-Toast-2.jpg");
+  }
+   if (message.content.startsWith(prefix + "toastbot")) {
+    message.channel.send("toastbot version 1.0.9");
+   }
+   
+if (message.content.startsWith(prefix + "hentaiass")) {
+    DabiClient.nsfw.hentai.ass().then(json => {
+	message.channel.send(Json);
+
+   
+  
+  
+  
+}); 
+}  
+  
+  
+});
+
+
+
+
+client.login(process.env.token);
+
+
