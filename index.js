8e95c1f896ae62f197af87ddd399c1232b24a4f9
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2020-07-21 17:55:02 -0700

    stuff

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const prefix = 't/';
 const fetch = require('node-fetch');
 const randomPuppy = require('random-puppy');
 var subreddit = "hentai";
-
+const got = require('got');
 
 client.on("message", (message) => {
   // Exit and stop if it's not there
@@ -21,10 +21,25 @@ client.on("message", (message) => {
    if (message.content.startsWith(prefix + "toastbot")) {
     message.channel.send("toastbot version 1.1.0");
    }
-   if (message.content.startsWith(prefix + "puppy")) {
-   randomPuppy('shittyrainbow6')
-    .then(url => {
-        message.channel.send(url);
+   if (message.content.startsWith(prefix + "meme")) {
+		   const embed = new Discord.RichEmbed();
+    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeDownvotes = content[0].data.children[0].data.downs;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.addField(`${memeTitle}`, `[View thread](${memeUrl})`);
+        embed.setImage(memeImage);
+        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`);
+        msg.channel.send(embed)
+            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+        console.log('Bot responded with: ' + memeImage);
+    }).catch(console.error);
+}
     })
 
 }
