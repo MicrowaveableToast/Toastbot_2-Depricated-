ff8df3ab0eb7823984be1cd37f0bcdec4b2c1566
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2020-09-01 14:11:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === Tag+'ping') {
     msg.reply('Pong!')
+  }
   if (msg.content === Tag+'MakeMaddoxAdmin') {
     msg.send('Pong!')
   }
