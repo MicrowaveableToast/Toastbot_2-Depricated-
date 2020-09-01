ffadae158f193985fe69d39ee15dacd248b8e67f
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2020-09-01 14:01:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Tag = 't/'
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === Tag+'ping') {
     msg.reply('Pong!');
   }
 });
