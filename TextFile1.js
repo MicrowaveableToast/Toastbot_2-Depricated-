dadd82cfd007254563edd889c69b8baeadfdcd59
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-01-05 15:05:37 -0800

    Suff

---
+++ TextFile1.js
@@ -0,0 +1,198 @@
+if (command == 'hentai') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.hentai());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.hentai()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'nsfwneko') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.neko());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.nsfwNeko()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'yuri') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.yuri());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.yuri()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'femdom') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.femdom());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.femdom()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'feet') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.foot());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.feet()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'pussy') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.vagina());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.pussy()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'ero') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.ero());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.ero()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'blowjob') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.blowjob());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.blowjob()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'masterbation') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.masterbation());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.pwankg()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'cum') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.cum());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.cum()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
+if (command == 'ahegao') {
+    if (message.channel.nsfw) {
+        let numb = Math.floor(Math.random() * 2) + 1
+        if (numb == 1) {
+            embed.setImage(HMfull.HMtai.nsfw.ahegao());
+            return message.channel.send(embed);
+        }
+        if (numb == 2) {
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.gasm()
+                message.channel.send(res.url);
+            }
+            Nekos();
+
+        }
+
+    }
+}
--- app.js
+++ app.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
     console.log('TOASTBOT READY');
 });
 
-client.on('message', async message => {
+client.on('message', message => {
     const embed = new Discord.MessageEmbed();
 
     var command = message.content.toLowerCase().slice(settings.prefix.length).split(" ")[0];
@@ -50,13 +50,7 @@ client.on('message', async message => {
         }
 
     }
-    if (command == 'cum') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.cum());
-            return message.channel.send(embed);
-        }
-
-    }
+  
     if (command == 'manga') {
         if (message.channel.nsfw) {
             embed.setImage(hmtai.nsfw.manga());
@@ -64,34 +58,10 @@ client.on('message', async message => {
         }
 
     }
-    if (command == 'femdom') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.femdom());
-            return message.channel.send(embed);
-        }
-
-    }
-    if (command == 'hentai') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.hentai());
-            return message.channel.send(embed);
-        }
-
-    }
-    if (command == 'masturbation') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.masturbation());
-            return message.channel.send(embed);
-        }
-
-    }
-    if (command == 'ero') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.ero());
-            return message.channel.send(embed);
-        }
-
-    }
+   
+    
+    
+    
     if (command == 'orgy') {
         if (message.channel.nsfw) {
             embed.setImage(hmtai.nsfw.orgy());
@@ -99,13 +69,7 @@ client.on('message', async message => {
         }
 
     }
-    if (command == 'yuri') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.yuri());
-            return message.channel.send(embed);
-        }
-
-    }
+    
     if (command == 'pantsu') {
         if (message.channel.nsfw) {
             embed.setImage(hmtai.nsfw.pantsu());
@@ -127,20 +91,8 @@ client.on('message', async message => {
         }
 
     }
-    if (command == 'blowjob') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.blowjob());
-            return message.channel.send(embed);
-        }
-
-    }
-    if (command == 'foot') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.foot());
-            return message.channel.send(embed);
-        }
-
-    }
+   
+   
     if (command == 'thighs') {
         if (message.channel.nsfw) {
             embed.setImage(hmtai.nsfw.thighs());
@@ -148,20 +100,8 @@ client.on('message', async message => {
         }
 
     }
-    if (command == 'vagina') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.vagina());
-            return message.channel.send(embed);
-        }
-
-    }
-    if (command == 'ahegao') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.ahegao());
-            return message.channel.send(embed);
-        }
-
-    }
+   
+    
     if (command == 'uniform') {
         if (message.channel.nsfw) {
             embed.setImage(hmtai.nsfw.uniform());
@@ -192,12 +132,7 @@ client.on('message', async message => {
             message.reply('BONK go to horny jail');
         }
     }
-    if (command == 'nsfwneko') {
-        if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.nsfwNeko());
-            return message.channel.send(embed);
-        }
-    }
+   
 
     if (command == 'zettaiRyouiki') {
         if (message.channel.nsfw) {
@@ -211,81 +146,327 @@ client.on('message', async message => {
             embed.setImage(hmtai.nsfw.nsfwMobileWallpaper());
             return message.channel.send(embed);
         }
-
-
-
     }
+
+
+
+
+ 
+        
+    
+    
+    
     if (command == 'kuni') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.kuni());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.kuni()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
+     }
+
+    
+
     if (command == 'boobs') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.boobs());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.boobs()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
+     }
     if (command == 'anal') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.anal());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.anal()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
+     }
     if (command == 'trap') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.trap());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.trap()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
-    if (command == 'tits') {
+     }
+
+    
+
+    
+       if (command == 'tits') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.tits());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.tits()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
-    if (command == 'lewdneko') {
+     }
+
+    
+
+    
+       if (command == 'keta') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.lewdneko());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.keta()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
-    if (command == 'keta') {
+     }
+
+    
+
+
+    
+       if (command == 'nsfwavatar') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.keta());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.avatar()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
-    if (command == 'nsfwavatar') {
+     }
+
+    
+
+    if (command == 'wallpaper') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.avatar());
-            return message.channel.send(embed);
+            async function Nekos() {
+                let res = await HMfull.Nekos.nsfw.wallpaper()
+                message.channel.send(res.url); 
+            } 
+            Nekos();
         }
 
-    }
-    if (command == 'nsfwwallpaper') {
+     }
+    if (command == 'hentai') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.wallpaper());
-            return message.channel.send(embed);
-        }
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.hentai());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.hentai()
+                    message.channel.send(res.url);
+                }
+                Nekos();
 
+            }
+
+        }
     }
-    if (command == 'futa') {
+    if (command == 'nsfwneko') {
         if (message.channel.nsfw) {
-            embed.setImage(HMfull.Nekos.nsfw.futarani());
-            return message.channel.send(embed);
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.neko());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.nsfwNeko()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
         }
-
     }
+    if (command == 'yuri') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.yuri());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.yuri()
+                    message.channel.send(res.url);
+                }
+                Nekos();
 
+            }
+
+        }
+    }
+    if (command == 'femdom') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.femdom());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.femdom()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'feet') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.foot());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.feet()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'pussy') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.vagina());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.pussy()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'ero') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.ero());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.ero()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'blowjob') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.blowjob());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.blowjob()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'masterbation') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.masterbation());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.pwankg()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'cum') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.cum());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.cum()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    if (command == 'ahegao') {
+        if (message.channel.nsfw) {
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(HMfull.HMtai.nsfw.ahegao());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+                async function Nekos() {
+                    let res = await HMfull.Nekos.nsfw.gasm()
+                    message.channel.send(res.url);
+                }
+                Nekos();
+
+            }
+
+        }
+    }
+    
+
+   
+
+    
+
+
+    
+
+
+
+ 
+    
 }
+
 );
 client.login(process.env.DISCORDJS_BOT_TOKEN);
