aeccc8e40e4fb235293c917ffb4450c5f9460b6e
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-01-21 00:22:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ client.on('message', message => {
         let idcheckchannel2 = client.channels.cache.get(move2)
         if (!args[0]) return message.channel.send('Please mention user and voice channel ID/IDs')
         if (!args[1]) return message.channel.send('Please mention voice channel ID/IDs')
-
+        if (args == 'horny') (
+            mem.voice.setChannel(`737890062347272206`)
         if(args[0] === 'everyone' && !move2) {
         let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
         for (const [memberID, member] of channel.members)
