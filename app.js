5512e8eed327bb4f2a43a2a72e1d4fcfb6147702
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-01-20 23:35:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ client.on('message', message => {
     if (message.author.bot) return;
     if (command == 'bonk') {
       message.channel.send('NO horny');
-      GuildMember.voice.setChannel('HornyJail')
+      ClientUser.setAFK();
       
     }
 
