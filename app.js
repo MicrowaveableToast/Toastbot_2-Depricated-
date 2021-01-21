188e046061e0efd54ae2851625b57aad3bee6567
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-01-20 23:50:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ client.on('message', message => {
     if (message.author.bot) return;
     if (command == 'bonk') {
       message.channel.send('NO horny');
-      user = message.guild.members.get(args.slice(0, 1).join(' '));
       user = user.user;
       user.voice.setChannel("Genaral")
       
