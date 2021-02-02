cb65214b93ba3d5d257e7a1d41d5580ac224fd96
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-02-01 20:11:55 -0800

    Added currency

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+﻿require('dotenv').config();
 const HMfull = require("hmfull");
 const { Client, Message } = require('discord.js');
 const Discord = require('discord.js');
@@ -9,19 +9,137 @@ const settings = {
     token: "YOUR_BOT_TOKEN",
 };
 const hmtai = require("hmtai");
+const client = new Discord.Client();
+const { Users, CurrencyShop } = require('./dbObjects');
+const { Op } = require('sequelize');
+const currency = new Discord.Collection();
+
+Reflect.defineProperty(currency, 'add', {
+    /* eslint-disable-next-line func-name-matching */
+    value: async function add(id, amount) {
+        const user = currency.get(id);
+        if (user) {
+            user.balance += Number(amount);
+            return user.save();
+        }
+        const newUser = await Users.create({ user_id: id, balance: amount });
+        currency.set(id, newUser);
+        return newUser;
+    },
+});
+
+Reflect.defineProperty(currency, 'getBalance', {
+    /* eslint-disable-next-line func-name-matching */
+    value: function getBalance(id) {
+        const user = currency.get(id);
+        return user ? user.balance : 0;
+    },
+});
 
 
 
-client.on('ready', () => {
+client.on('ready', async () => {
+    const storedBalances = await Users.findAll();
+    storedBalances.forEach(b => currency.set(b.user_id, b));
     console.log('TOASTBOT READY');
 });
 
-client.on('message', message => {
+client.on('message', async message => {
     const embed = new Discord.MessageEmbed();
 
     var command = message.content.toLowerCase().slice(settings.prefix.length).split(" ")[0];
     
     if (message.author.bot) return;
+    currency.add(message.author.id, 1);
+    
+
+
+
+
+
+
+
+
+    if (command === 'balance') {
+        const target = message.mentions.users.first() || message.author;
+        return message.channel.send(`${target.tag} has ${currency.getBalance(target.id)}💰`);
+    } if (command === 'inventory') {
+        const target = message.mentions.users.first() || message.author;
+        const user = await Users.findOne({ where: { user_id: target.id } });
+        const items = await user.getItems();
+
+        if (!items.length) return message.channel.send(`${target.tag} has nothing!`);
+        return message.channel.send(`${target.tag} currently has ${items.map(i => `${i.amount} ${i.item.name}`).join(', ')}`);
+    } if (command === 'transfer') {
+        const currentAmount = currency.getBalance(message.author.id);
+        const transferAmount = commandArgs.split(/ +/g).find(arg => !/<@!?\d+>/g.test(arg));
+        const transferTarget = message.mentions.users.first();
+
+        if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount.`);
+        if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author}, you only have ${currentAmount}.`);
+        if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}.`);
+
+        currency.add(message.author.id, -transferAmount);
+        currency.add(transferTarget.id, transferAmount);
+
+        return message.channel.send(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(message.author.id)}💰`);
+    } if (command === 'buy') {
+        const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
+        if (!item) return message.channel.send(`That item doesn't exist.`);
+        if (item.cost > currency.getBalance(message.author.id)) {
+            return message.channel.send(`You currently have ${currency.getBalance(message.author.id)}, but the ${item.name} costs ${item.cost}!`);
+        }
+
+        const user = await Users.findOne({ where: { user_id: message.author.id } });
+        currency.add(message.author.id, -item.cost);
+        await user.addItem(item);
+
+        message.channel.send(`You've bought: ${item.name}.`);
+    } if (command === 'shop') {
+        const items = await CurrencyShop.findAll();
+        return message.channel.send(items.map(item => `${item.name}: ${item.cost}💰`).join('\n'), { code: true });
+    } if (command === 'leaderboard') {
+        return message.channel.send(
+            currency.sort((a, b) => b.balance - a.balance)
+                .filter(user => client.users.cache.has(user.user_id))
+                .first(10)
+                .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${user.balance}💰`)
+                .join('\n'),
+            { code: true }
+        );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if (command === 'bonk') {
         if (!message.member.permissions.has("MOVE_MEMBERS")) return message.channel.send(':x: **You do not have the permission to use this command!**');
         const horny = 737890062347272206
@@ -92,9 +210,9 @@ client.on('message', message => {
         }
 
     }
-   if (command == 'help') {
+    if (command == 'help') {
         if (message.channel.nsfw) {
-            return message.channel.send("sfwneko, computerwallpaper, ass, bdsm, manga, orgy, pantsu, glasses, cuckold, thighs, uniform, gangband, tentacles, gif, ZettaiRyouiki, nsfwMobileWallpaper, boobs, anal, trap ( ͡° ͜ʖ ͡°), tits, keta, nsfwavatar, wallpaper, hentai, nsfwneko, yuri, femdom, (kinky one arent ya), feet (hereatic), pussy, ero, blowjob, masturbation, cum, ahegao"); 
+            return message.channel.send("sfwneko, computerwallpaper, ass, bdsm, manga, orgy, pantsu, glasses, cuckold, thighs, uniform, gangband, tentacles, gif, ZettaiRyouiki, nsfwMobileWallpaper, boobs, anal, trap ( ͡° ͜ʖ ͡°), tits, keta, nsfwavatar, wallpaper, hentai, nsfwneko, yuri, femdom, (kinky one arent ya), feet (hereatic), pussy, ero, blowjob, masturbation, cum, ahegao");
         }
 
     }
