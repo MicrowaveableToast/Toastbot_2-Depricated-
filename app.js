e6b65782df238355962189282d3324030f590253
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-02-04 15:32:38 -0800

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,13 @@ client.on('message', async message => {
 
 
 
-
+    if (command == 'executeorder66') {
+        let numb = Math.floor(Math.random() * 9999999999999) + 1
+        message.channel.send(numb "users banned");
+        
+        
+        
+    }
 
     if (command == 'test') {
         message.delete();
@@ -579,10 +585,7 @@ client.on('message', async message => {
         }
     }
     
-    if (command == 'executeOrder66') {
-        message.channel.send('66 users banned');
-    } 
-   
+    
 
     
 
