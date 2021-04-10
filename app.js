f23e13759a4608edd4c4b23bfcb722050bf77834
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-04-10 14:20:26 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+﻿
 ﻿require('dotenv').config();
 const HMfull = require("hmfull");
 const { Client, Message } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Client();
 const prefix = "t/";
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
 const settings = {
     prefix: "t/",
     
 };
 const hmtai = require("hmtai");
+const nHentai = require("@v0idpointer/nhentai.js");
 
-
-
-
-
+const { API, } = require('nhentai-api');
+const api = new API();
 client.on('ready', async () => {
     
     console.log('TOASTBOT READY');
@@ -42,8 +44,8 @@ client.on('message', async message => {
 		
 		
 		
-		
-		
+	
+	
 		
 		
 		
@@ -89,25 +91,32 @@ client.on('message', async message => {
        
        
    }
-   if (command == 'devme') {
- 	if (!message.member.permissions.has("MANAGE_ROLES")) return message.channel.send(':x: **You do not have the permission to use this command!**');
-	const eee = message.mentions.members.first();
-	message.delete();   
-	let r1 = message.guild.roles.cache.get('808945417706012717');
-	if(eee == null) return message.reply('you retard')
-	eee.roles.add(r1)
-	
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
     if (command == 'executeorder66') {
         var numb = Math.floor(Math.random() * 999999999999) + 1
         message.channel.send(numb + " users banned");
@@ -175,8 +184,18 @@ client.on('message', async message => {
     if (command == 'ass') {
         message.delete();
         if (message.channel.nsfw) {
-            embed.setImage(hmtai.nsfw.ass());
-            return message.channel.send(embed);
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 2) {
+                embed.setImage(hmtai.nsfw.ass());
+                return message.channel.send(embed);
+            }
+            if (numb == 1){
+                const image = await nsfw.hentaiass();
+                const embed = new Discord.MessageEmbed()
+                .setImage(image);
+                message.channel.send(embed)
+                
+            }
         }
 
     }
@@ -200,7 +219,7 @@ client.on('message', async message => {
     if (command == 'help') {
         if (message.channel.nsfw) {
             let res = HMfull.HMtai.sfw.neko()
-            return message.channel.send("sfwneko, computerwallpaper, ass, bdsm, manga, orgy, pantsu, glasses, cuckold, thighs, uniform, gangband, tentacles, gif, ZettaiRyouiki, nsfwMobileWallpaper, boobs, anal, trap ( ͡° ͜ʖ ͡°), tits, keta, nsfwavatar, wallpaper, hentai, nsfwneko, yuri, femdom, (kinky one arent ya), feet (hereatic), pussy, ero, blowjob, masturbation, cum, ahegao");
+            return message.channel.send("4k, irlass, irlpussy, irlboobs, irlthighs, irllewd, irlgif,  sfwneko, mid,  computerwallpaper, ass, bdsm, manga, orgy, pantsu, glasses, cuckold, thighs, uniform, gangband, tentacles, gif, ZettaiRyouiki, nsfwMobileWallpaper, boobs, irlanal, trap ( ͡° ͜ʖ ͡°), tits, nekotits, nekofeet, neko pussy keta, nsfwavatar, wallpaper, hentai, nsfwneko, yuri, femdom, (kinky one arent ya), feet (hereatic), pussy, ero, blowjob, masturbation, cum, ahegao");
             message.channel.send(res.url);
         }
 
@@ -246,8 +265,17 @@ client.on('message', async message => {
     if (command == 'thighs') {
         if (message.channel.nsfw) {
             message.delete();
-            embed.setImage(hmtai.nsfw.thighs());
-            return message.channel.send(embed);
+            let numb = Math.floor(Math.random() * 2) + 1
+            if (numb == 1) {
+                embed.setImage(hmtai.nsfw.thighs());
+                return message.channel.send(embed);
+            }
+            if (numb == 2) {
+               const image = await nsfw.hentaithigh();
+               const embed = new Discord.MessageEmbed()
+               .setImage(image);
+               message.channel.send(embed)     
+            }
         }
 
     }
@@ -305,12 +333,42 @@ client.on('message', async message => {
         }
     }
 
+     
+    if (command == 'irlgif') {
+        const image = await nsfw.pgif();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+    }
 
-
-
- 
+    if (command == 'irlpussy') {
+        const image = await nsfw.pussy();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
         
-    
+    }
+    if (command == "irlthigh")   {
+        const image = await nsfw.thigh();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+
+
+    }
+    if (command == "irlboobs") {
+        const image = await nsfw.boobs();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+
+    }
+    if (command == 'irllewd') {
+        const image = await nsfw.lewd();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+    }
     
     
     if (command == 'kuni') {
@@ -325,7 +383,13 @@ client.on('message', async message => {
 
      }
 
-    
+    if (command == 'irlass') {
+
+        const image = await nsfw.ass();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+    }
 
     if (command == 'boobs') {
         if (message.channel.nsfw) {
@@ -338,16 +402,11 @@ client.on('message', async message => {
         }
 
      }
-    if (command == 'anal') {
-        if (message.channel.nsfw) {
-            async function Nekos() {
-                message.delete();
-                let res = await HMfull.Nekos.nsfw.anal()
-                message.channel.send(res.url); 
-            } 
-            Nekos();
-        }
-
+     if (command == "irlgonewild") {
+        const image = await nsfw.gonewild();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
      }
     if (command == 'trap') {
         if (message.channel.nsfw) {
@@ -407,8 +466,46 @@ client.on('message', async message => {
 
      }
 
-    
+    if (command == '4k') {
+        const image = await nsfw.fourk();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
 
+
+    }
+    if (command == 'nekofeet') {
+        const image = await nsfw.nekofeet();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+
+
+    }
+    if (command == "mid") {
+        const image = await nsfw.hmidriff();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed)
+
+    }
+    if (command == 'nekopussy'){
+        const image = await nsfw.nekopussy();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+
+
+
+    }
+    if (command == 'nekotits') {
+        const image = await nsfw.nekotits();
+        const embed = new Discord.MessageEmbed()
+        .setImage(image);
+        message.channel.send(embed);
+
+
+    }
     if (command == 'wallpaper') {
         if (message.channel.nsfw) {
             async function Nekos() {
@@ -420,11 +517,32 @@ client.on('message', async message => {
         }
 
      }
+     if (command == 'irlanal') {
+        if (message.channel.nsfw) {
+            message.delete();
+            let numb = Math.floor(Math.random() * 2) + 1
+            async function Nekos() {
+                
+                let res = await HMfull.Nekos.nsfw.anal()
+                message.channel.send(res.url); 
+            } 
+            
+            if (numb >0) {
+                const image = await nsfw.anal();
+                const embed = new Discord.MessageEmbed()
+                embed.setImage(image);
+                message.channel.send(embed); 
+            
+                
+            }
+        }
+
+     }
     if (command == 'hentai') {
         if (message.channel.nsfw) {
             message.delete();
-            let numb = Math.floor(Math.random() * 3) + 1
-            if (numb >= 2) {
+            let numb = Math.floor(Math.random() * 5) + 1
+            if (numb = 2) {
                 let res = HMfull.HMtai.nsfw.hentai()
                 message.channel.send(res.url);
             }
@@ -434,6 +552,13 @@ client.on('message', async message => {
                     message.channel.send(res.url);
                 }
                 Nekos();
+
+            }
+            if (numb >= 3) {
+              const image = await nsfw.hentai();
+              const embed = new Discord.MessageEmbed()
+              .setImage(image);
+              message.channel.send(embed)
 
             }
 
@@ -645,4 +770,5 @@ client.on('message', async message => {
 
 );
 
-client.login(process.env.TOKEN);
+//client.login(process.env.TOKEN);
+client.login('NzkzOTczMTc2NjE0NjQ5OTQ2.X-0DJA.3J1Wb_ogtYNM74OtdpixvbrPqAk');
