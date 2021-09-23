47ae9f4b0293ba0f397087257315405e015a7e89
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2021-09-22 22:30:51 -0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -86,7 +86,7 @@ const video_player = async (guild, song) => {
     const song_queue = queue.get(guild.id);
 
     //If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
-    if (!song) {
+    if (!song || !queue) {
         song_queue.voice_channel.leave();
         queue.delete(guild.id);
         return;
